23fda94fbbd593a5a0f0106f57a4bd65942e71a5
Sanghoon, Kim <xsemiyas@gmail.com>
2024-01-25 18:58:39 +0900

    add FullScreen

---
+++ app/monitoring/common/FullScreen.tsx
@@ -0,0 +1,41 @@
+
+export const fullscreen = (element: any) => {
+    if (element.requestFullscreen) return element.requestFullscreen()
+    else if (element.webkitRequestFullscreen) return element.webkitRequestFullscreen()
+    else if (element.mozRequestFullScreen) return element.mozRequestFullScreen()
+    else if (element.msRequestFullscreen) return element.msRequestFullscreen()
+}
+
+export const exitFullScreen = () => {
+    const documentElement = document as unknown as { 
+        exitFullscreen: () => void;
+        webkitCancelFullscreen: () => void;
+        mozCancelFullScreen: () => void;
+        msExitFullscreen: () => void;
+    };
+    if (documentElement.exitFullscreen) return documentElement.exitFullscreen()
+    else if (documentElement.webkitCancelFullscreen) return documentElement.webkitCancelFullscreen()
+    else if (documentElement.mozCancelFullScreen) return documentElement.mozCancelFullScreen()
+    else if (documentElement.msExitFullscreen) return documentElement.msExitFullscreen()
+}
+
+export const toggleFullScreen = (element: any) => {
+    const documentElement = document as unknown as { 
+        exitFullscreen: () => void;
+        webkitCancelFullscreen: () => void;
+        mozCancelFullScreen: () => void;
+        msExitFullscreen: () => void;
+    };
+    if (!document.fullscreenElement) {
+        if (element.requestFullscreen) return element.requestFullscreen()
+        else if (element.webkitRequestFullscreen) return element.webkitRequestFullscreen()
+        else if (element.mozRequestFullScreen) return element.mozRequestFullScreen()
+        else if (element.msRequestFullscreen) return element.msRequestFullscreen()
+    } else {
+        if (documentElement.exitFullscreen) return documentElement.exitFullscreen()
+        else if (documentElement.webkitCancelFullscreen) return documentElement.webkitCancelFullscreen()
+        else if (documentElement.mozCancelFullScreen) return documentElement.mozCancelFullScreen()
+        else if (documentElement.msExitFullscreen) return documentElement.msExitFullscreen()
+    }
+}
+
