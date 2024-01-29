1df6585c80397fcfbe4bba7bef5fecdcc9a6794b
Sanghoon, Kim <xsemiyas@gmail.com>
2024-01-29 23:18:48 +0900

    checked fullscreen

--- app/monitoring/layout.tsx
+++ app/monitoring/layout.tsx
@@ -4,6 +4,7 @@ import '../globals.css'
 import Footer from './layouts/Footer'
 import React from 'react'
 import { Col, Container, Row } from 'reactstrap'
+// import { fullscreen, exitFullScreen} from './common/FullScreen'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,8 @@ export default function MonitoringLayout(props: {
     return (
         <React.Fragment>
             <div id="layout-wrapper">
+            {/* <button onClick={()=> fullscreen(document.getElementById("layout-wrapper"))}>Make FullScreen</button>
+            <button onClick={() => exitFullScreen()}>Exit FullScreen</button> */}
                 {props.children}
                 <Container fluid>
                     <Row>
